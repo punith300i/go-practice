9b67077870e9dc36327b6d57b5494fcd17e26935
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2024-03-15 03:47:22 -0700

    Deck Comleted

--- cards/deck.go
+++ cards/deck.go
@@ -1,11 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 // type deck : slice of strings
 type deck []string
 
-func newDeck() deck{
+func newDeck() deck {
 	cards := deck{}
 
 	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
@@ -13,7 +19,7 @@ func newDeck() deck{
 
 	for _, cardSuit := range cardSuits {
 		for _, cardValue := range cardValues {
-			cards = append(cards, cardValue + " of " + cardSuit)
+			cards = append(cards, cardValue+" of "+cardSuit)
 		}
 	}
 
@@ -24,9 +30,36 @@ func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
 
-func (d deck) print(){
+func (d deck) print() {
 	for i, card := range d {
-		fmt.Println(i,card)
+		fmt.Println(i, card)
 	}
 }
 
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(fileName string) error {
+	return os.WriteFile(fileName, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(fileName string) deck {
+	bs, err := os.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Error", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPos := r.Intn(len(d) - 1)
+		d[i], d[newPos] = d[newPos], d[i]
+	}
+}
--- cards/main.go
+++ cards/main.go
@@ -1,15 +1,26 @@
 package main
 
-func main(){
+import (
+	"fmt"
+)
+
+func main() {
 	cards := newDeck()
 	cards.print()
 
 	hand, remainingCards := deal(cards, 5)
 	hand.print()
 	remainingCards.print()
+	fmt.Println(cards.toString())
+	cards.saveToFile("test.txt")
+	var newCards deck = newDeckFromFile("test.txt")
+	newCards.print()
+	newCards.shuffle()
+	fmt.Println("\nTest\n")
+	newCards.print()
 }
 
 func newCard() string {
 	return "5 of Diamonds"
-	
+
 }
